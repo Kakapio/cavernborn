e5202b0a0c9d67a3c15dd9e3914d9c802fd77d69
Roshan Patel <ovicior@gmail.com>
2024-09-29 15:43:43 -0700

    add debug graphic and logging msg

--- crates/game/src/main.rs
+++ crates/game/src/main.rs
@@ -17,4 +17,16 @@ fn main() {
 fn setup(mut commands: Commands) {
     // Add a 2D camera
     commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::srgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    });
+
+    info!("Starting game...");
 }
