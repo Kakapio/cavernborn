e90a279bb168f73273cb1fdc7a4e0b6441e28763
Roshan Patel <ovicior@gmail.com>
2024-09-29 01:58:44 -0700

    dyn linking for bevy, open window

--- crates/game/src/main.rs
+++ crates/game/src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, Cavernborn!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "My Bevy Window".into(),
+                resolution: (800., 600.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // Add a 2D camera
+    commands.spawn(Camera2dBundle::default());
 }
